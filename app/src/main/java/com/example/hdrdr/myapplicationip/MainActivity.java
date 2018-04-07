cd78d308d8d3a91d96ffc9b6a96964656661a6d2
HDRDR <00400815@uca.edu.sv>
2018-04-07 14:51:43 -0600

    Initial commit: > creando la interfaz grafica de la aplicacion

---
+++ app/src/main/java/com/example/hdrdr/myapplicationip/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hdrdr.myapplicationip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn1;
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = (Button) findViewById(R.id.btn);
+        tv1 = (TextView) findViewById(R.id.et_ip);
+
+
+
+
+
+
+    }
+}
