6a08f119314fe2b5f3294fe1a2bdb2af15d1ba3a
HDRDR <00400815@uca.edu.sv>
2018-04-07 16:12:05 -0600

    se ha agregado metodos para el calculo de broad cast

--- app/src/main/java/com/example/hdrdr/myapplicationip/MainActivity.java
+++ app/src/main/java/com/example/hdrdr/myapplicationip/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.hdrdr.myapplicationip;
+import control.nose;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn1;
-    private TextView tv1;
+    private EditText etIp;
+    private EditText etMask;
+    private EditText etBroadcast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +20,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn1 = (Button) findViewById(R.id.btn);
-        tv1 = (TextView) findViewById(R.id.et_ip);
+        etIp = (EditText) findViewById(R.id.et_ip);
+        etMask = (EditText) findViewById(R.id.et_mask);
+        etBroadcast = (EditText) findViewById(R.id.et_broad);
 
 
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //String q= etIp.getText();
+            //String w= "";
+                //tv1.setText(Integer.toString(counterA));
+                //nose.calcular(String.valueOf(etIp.getText()) , String.valueOf(etMask.getText()));
+                //etIp.setText("gh");
+                etBroadcast.setText(String.valueOf(nose.calcular(String.valueOf(etIp.getText()) , String.valueOf(etMask.getText()))));
+                //etBroadcast.setText("dffdfddf");
+
+
+            }
+        });
 
 
 
---
+++ app/src/main/java/control/nose.java
@@ -0,0 +1,105 @@
+package control;
+import com.example.hdrdr.myapplicationip.MainActivity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HDRDR on 4/7/2018.
+ */
+
+public class nose {
+
+    public static String calcular (String ip, String masc){char dot ='.';
+        int cont=3;
+
+
+        String octeDec="";
+        ArrayList<Integer> ipDecimal = new ArrayList<Integer>();
+        ArrayList<Integer> dirRed = new ArrayList<Integer>();
+
+
+
+
+        for(int n = 0 ; n<=ip.length()-1 ; n++) {                                   //
+            if (ip.charAt(n) != dot) {
+                octeDec+=Character.toString(ip.charAt(n));
+            }
+            if (ip.charAt(n) == dot) {
+                ipDecimal.add(Integer.valueOf(octeDec));
+
+                octeDec="";
+            }
+        }                                                                           //
+        ipDecimal.add(Integer.valueOf(octeDec));
+
+
+
+
+        ArrayList<Integer> ipBinaria = new ArrayList<Integer>();
+
+        String octa="";
+        int num = 0;
+
+        for (int f = 0; f < ipDecimal.size(); f++) {
+            octa=(Integer.toString(ipDecimal.get(f))); //error
+
+            int z = Integer.valueOf(octa);
+            octa=Integer.toString(z, 2);
+
+            String octaInv="";
+            for (int j=octa.length()-1 ; j>=0 ; j--) octaInv = octaInv + octa.charAt(j); //invertir la cadena con el octeto en binario
+
+
+            for (int i = (8-octaInv.length()); i > 0; i--) octaInv+="0";    //agregar ceros al octeto
+
+
+            octa="";
+            for (int j=octaInv.length()-1 ; j>=0 ; j--) octa = octa + octaInv.charAt(j); //revertir la cadena contenido el octeto con ceros a la izquierda en binario
+
+            for (int i = 0; i < 8; i++) {
+                ipBinaria.add(Integer.parseInt(String.valueOf(octa.charAt(i))));
+            }
+
+            octa="";
+        }
+
+        for (int i = 0; i < Integer.parseInt(masc); i++) {
+            dirRed.add(ipBinaria.get(i));
+
+        }
+
+
+
+        for (int i = Integer.parseInt(masc); i < 32-1 ; i++) {
+            dirRed.add(1);
+
+        }
+        dirRed.add(0);
+
+
+        String UltimoHost = "";
+        octa="";
+        cont = 0;
+        num = 0;
+        for (int i = 0; i < 32; i++) {
+            cont++;
+            octa += Integer.toString(dirRed.get(i));
+
+            if (cont == 8) {
+                num++;
+
+                int z = 0;
+                z = Integer.parseInt(octa, 2);
+
+                UltimoHost += z;
+                if (num<4) UltimoHost += ".";
+
+                cont = 0;
+                octa = "";
+            }
+        }
+        System.out.println("direccion de ultimo host: "+UltimoHost );
+
+        return UltimoHost;
+    }
+}
